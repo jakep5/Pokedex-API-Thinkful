18fbe118ce401a932dbb76805fd9e9556cd01717
jakep5 <jakepagel1@yahoo.com>
2019-11-06 10:25:51 -0600

    add beginner validation function, add both get routes

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
+require('dotenv').config();
 
 const app = express();
+
+console.log(process.env.API_TOKEN)
 
 app.use(morgan('dev'))
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
 
+app.use(function validateBearerToken(req, res, next) {
+    console.log('validate bearer token middleware')
+    //move to next middleware/callback function
+    next(); //if this is not included, request would just hang here and not continue on - IMPORTANT*****
+})
+
 function handleGetTypes(req, res) {
-
-
+    res.json(validTypes)
 }
 
 app.get('/types', handleGetTypes) //the get request to /types endpoint gets routed to the handleGetTypes function
+
+app.get('/pokemon', handleGetPokemon)
+
+function handleGetPokemon(req, res) {
+    res.send('Hello, Pokemon!');
+}
 
 app.use((req, res) => {
     res.send('Hello, world! What is up?')
