8d4d85b2bfeda2098415dca4f506a3d4c748be0a
jakep5 <jakepagel1@yahoo.com>
2019-11-06 10:50:53 -0600

    add validate key function

--- server.js
+++ server.js
@@ -11,8 +11,13 @@ app.use(morgan('dev'))
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
 
 app.use(function validateBearerToken(req, res, next) {
+    const bearerToken = req.get('Authorization').split(' ')[1];
+    const apiToken = process.env.API_TOKEN;
     console.log('validate bearer token middleware')
     //move to next middleware/callback function
+    if (bearerToken !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request'}) //this will ensure that execution stops if provided API key does not match environmental variable
+    };
     next(); //if this is not included, request would just hang here and not continue on - IMPORTANT*****
 })
 
